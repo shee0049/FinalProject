ac9f063d69a08eb1e706e630aa5fcf297ac6f824
Jama0096 <jama0096@algonquinlive.com>
2018-01-08 11:31:54 -0500

    Changed line 12 on index

--- Index.php
+++ Index.php
@@ -9,7 +9,7 @@ if (isset($_SESSION["username"])){
 else {
     require 'Common/Header.php';
     echo "<div class='container'>";
-    echo "<h1>Welcome to the Algonquin Social Media Website</h1>";
+    echo "<h1>Welcome to OUR the Algonquin Social Media Website</h1>";
     echo "<p>If this is your first time using click <a href='SignUp.php'>here!</a> to sign up</p>";
     echo "<p>Otherwise <a href='Login.php'>here</a> to log in</p>";
     echo "</div>";
