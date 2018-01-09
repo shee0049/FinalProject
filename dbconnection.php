a1150f6e71268d2bbf9296f316c976afd7e992ca
Jama0096 <jama0096@algonquinlive.com>
2018-01-09 11:56:48 -0500

    Edited up dbconnection, had to connect twice due to errors

--- dbconnection.php
+++ dbconnection.php
@@ -1,5 +1,14 @@
 <?php
 
+
+    $dbConn = new PDO('mysql:host=localhost; dbname=CST8257', 'PHPSCRIPT', '1234'
+                ,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                ,PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+
+    $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
+
+
 define('DB_SERVER', 'localhost');
 
 define('DB_USERNAME', 'PHPSCRIPT');
