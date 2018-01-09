9d74e26ad75c7747b9d55510a6f5a3f134cffbe0
Jama0096 <jama0096@algonquinlive.com>
2018-01-09 11:44:34 -0500

    Changed line 12 on index

--- AddAlbum.php
+++ AddAlbum.php
@@ -5,6 +5,7 @@ require '/Common/Loggedin.php';
 if (!isset($_SESSION["username"])){
     header("Location: Login.php");
 }
+
 ?>
 
 <div class="container">
@@ -13,10 +14,56 @@ if (!isset($_SESSION["username"])){
     
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
         <div class="form-group">
-            <label>Title: </label>
+            <label>Title: </label>       
             <input type="text" class="form-control" name="albumTitle">
-        </div>        
-    </form>
-</div>
+        </div> 
+        
+     <div>
+     <?php
+     // opening the Database and reading the table accessibility
+      require_once("dbconnection.php");
+      $request = $dbConn->prepare('SELECT * FROM accessibility');
+      $request->execute();
+      $cpt=0;      
+      $accCode = array();
+      $accDesc = array();
+      while ($data = $request->fetch())
+       {           
+        $accCode[$cpt] = $data['Accessibility_Code'];
+        $accDesc[$cpt] = $data['Description'];
+        $cpt++;
+       }      
+       
+       $request->closeCursor();
+       ?>
+        <label>Accessibility:</label>  
+       <select name="accessibility" id="accessibility" class="form-control">
+        <?php 
+        for ($i=0; $i<$cpt; $i++)
+        {
+         $accessibilityCode = $accCode[$i];
+         $description       = $accDesc[$i];
+        ?>
+           
+        <option value="<?php echo($accessibilityCode);?>">          
+         <?php echo($description);?>
+        </option>
+        <?php
+        }
+        ?>
+       </select>
+    </div>
+        
+   <div class="form-group">
+    <label>Description:</label>
+    <textarea class="form-control" id="description" name="description" class="form-control" rows="8"></textarea>
+   </div>
 
-<?php require '/Common/Footer.php'; ?>
+     <div>
+     <input type="submit" value="Submit" class="btn btn-large btn-primary" />
+     <input type="reset" value="Clear" class="btn btn-large btn-primary" />
+     </div>    
+        
+
+    </form>
+<?php require '/Common/Footer.php';?>
